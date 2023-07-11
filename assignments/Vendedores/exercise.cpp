30ced50c78301f290957db102dc641acb1c173d5
Roodrrigoo <110911226+Roodrrigoo@users.noreply.github.com>
2023-07-11 23:29:25 +0000

    definitivos

--- assignments/Vendedores/exercise.cpp
+++ assignments/Vendedores/exercise.cpp
@@ -2,112 +2,166 @@
 #include <fstream>
 using namespace std;
 #include "Vendedor.h"
-
-
 int buscaDepartamento(Depto listaDept[], int cantDept, int idDeptoABuscar) {
-    /*Función que busca un id de un departamento en el arreglo de departamentos
-    si lo encuentra que regrese la posición del arreglo donde está, si no regresa -1*/
-
-
+    // Función que busca un id de un departamento en el arreglo de departamentos
+    // Si lo encuentra, regresa la posición del arreglo donde está; si no, regresa -1
+    for (int i = 0; i < cantDept; i++) {
+        if (listaDept[i].getIdDep() == idDeptoABuscar) {
+            return i;
+        }
+    }
     return -1;
 }
 
-string nombreDepartamento(Depto listaDept[], int cantDept, int idDeptoABuscar) {
-    /*Función que busca un id de un departamento en el arreglo de departamentos
-      si lo encuentra que regrese el nombre del departamento, si no lo encuentra regresa ""*/
-    
+std::string nombreDepartamento(Depto listaDept[], int cantDept, int idDeptoABuscar) {
+    // Función que busca un id de un departamento en el arreglo de departamentos
+    // Si lo encuentra, regresa el nombre del departamento; si no, regresa ""
+    for (int i = 0; i < cantDept; i++) {
+        if (listaDept[i].getIdDep() == idDeptoABuscar) {
+            return listaDept[i].getNombre();
+        }
+    }
     return "";
 }
 
 void cargaDatosDepartamentos(Depto listaDeptos[], int &cantDeptos) {
-    /*Carga datos del archivo Deptos.txt en listaDeptos y cantDeptos lo actualiza
-      con el número de departamentos cargados del archivo*/
-    int numDepartamento;
-    string nombreDepartamento;
-
+    // Carga datos del archivo Deptos.txt en listaDeptos y actualiza cantDeptos
+    std::ifstream archivo("Deptos.txt");
+    if (archivo.is_open()) {
+        int idDep;
+        std::string nombreDep;
+        while (archivo >> idDep >> nombreDep) {
+            Depto departamento(idDep, nombreDep);
+            listaDeptos[cantDeptos] = departamento;
+            cantDeptos++;
+        }
+        archivo.close();
+    } else {
+        std::cout << "Error al abrir " << std::endl;
+    }
 }
 
 void muestraDepartamentos(Depto listaDept[], int cantDep) {
-    /*Muestra la lista de departamentos*/
-    cout << "Departamentos de la tienda" << endl;
-    
+    // Muestra la lista de departamentos
+    std::cout << "Departamentos de la tienda:" << std::endl;
+    for (int i = 0; i < cantDep; i++) {
+        listaDept[i].imprime();
+    }
 }
 
 void cargaDatosVendedores(Vendedor listaVend[], int &cantVend, Depto listaDep[], int cantDep) {
-    /* Función que regresa la listaVend y la cantVend con datos recibe el arreglo vacío
-       y la variable cantVend sin inicializar y les pone valor en esta función*/
-    int idDep;
-    string nom;
-    double vent;
-    int num;
-    
+    // Carga datos de los vendedores
+    std::cout << "¿Cuántos vendedores vas a ingresar? ";
+    std::cin >> cantVend;
+    for (int i = 0; i < cantVend; i++) {
+        std::string nombre;
+        double ventas;
+        int idDep;
+        std::cin.ignore();
+        std::cout << "Nombre: ";
+        std::getline(std::cin, nombre);
+        std::cout << "Ventas acumuladas: ";
+        std::cin >> ventas;
+        do {
+            std::cout << "Departamento: ";
+            std::cin >> idDep;
+        } while (buscaDepartamento(listaDep, cantDep, idDep) == -1);
+        listaVend[i] = Vendedor(nombre, ventas, listaDep[buscaDepartamento(listaDep, cantDep, idDep)]);
+    }
 }
 
 void muestraVendedores(Vendedor listaVen[], int cantVen) {
-    /*Muestra la lista de vendedores*/
-    
+    // Muestra la lista de vendedores
+    std::cout << "Vendedores:" << std::endl;
+    for (int i = 0; i < cantVen; i++) {
+        listaVen[i].imprime();
+    }
 }
 
 void vendedoresPorDepartamento(Vendedor listaVen[], int cantVend, Depto listaDep[], int cantDept, int idDeptoAConsultar) {
-    /* Muestra los nombres de los vendedores de un departamento */
-    
+    // Muestra los nombres de los vendedores de un departamento
+    std::cout << "Vendedores del departamento " << idDeptoAConsultar << ":" << std::endl;
+    for (int i = 0; i < cantVend; i++) {
+        if (listaVen[i].getDepartamento().getIdDep() == idDeptoAConsultar) {
+            std::cout << listaVen[i].getNombre() << std::endl;
+        }
+    }
 }
 
-int registrarVentas(Vendedor listaVend[], int cantVend) {
-    /* Registra las ventas de un vendedor de la lista de vendedores existentes
-       suma las ventas nuevas a las que ya estaban registradas */
+void registrarVentas(Vendedor listaVend[], int cantVend) {
+    // Registra las ventas de un vendedor de la lista de vendedores existentes
+    std::string nombreVend;
+    double ventas;
+    std::cout << "Ingrese el nombre del vendedor: ";
+    std::cin.ignore();
+    std::getline(std::cin, nombreVend);
+    int index = -1;
+    for (int i = 0; i < cantVend; i++) {
+        if (listaVend[i].getNombre() == nombreVend) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1) {
+        std::cout << "Ingrese las ventas a registrar: ";
+        std::cin >> ventas;
+        listaVend[index].setVentas(listaVend[index].getVentas() + ventas);
+        std::cout << "Ventas registradas correctamente." << std::endl;
+    } else {
+        std::cout << "Vendedor no encontrado." << std::endl;
+    }
 }
 
 int main() {
     char opcion;
     int numDepto;
     Vendedor listaVendedores[10];
-    int cantVendedores;  // cantidad de empleados en la lista de empleados
-    Depto listaDeptos[10]; // inicio el arreglo con Deptos default.
-    int cantDeptos = 0;  // cantidad de departamentos en la lista de deptos
+    int cantVendedores = 0;
+    Depto listaDeptos[10];
+    int cantDeptos = 0;
     cargaDatosDepartamentos(listaDeptos, cantDeptos);
     cargaDatosVendedores(listaVendedores, cantVendedores, listaDeptos, cantDeptos);
     do {
-        cout << endl;
-        cout << "Opciones disponibles " << endl;
-        cout << " 1) Ver la lista de Departamentos " << endl;
-        cout << " 2) Ver la lista de Vendedores " << endl;
-        cout << " 3) Consultar vendedores de un departamento " << endl;
-        cout << " 4) Registrar ventas "<< endl;
-        cout << " 5) Terminar " << endl;
-        cin >> opcion;
+        std::cout << std::endl;
+        std::cout << "Opciones disponibles:" << std::endl;
+        std::cout << " 1) Ver la lista de Departamentos" << std::endl;
+        std::cout << " 2) Ver la lista de Vendedores" << std::endl;
+        std::cout << " 3) Consultar vendedores de un departamento" << std::endl;
+        std::cout << " 4) Registrar ventas" << std::endl;
+        std::cout << " 5) Terminar" << std::endl;
+        std::cin >> opcion;
         switch (opcion) {
             case '1':
                 muestraDepartamentos(listaDeptos, cantDeptos);
-                cin.ignore();
-                cout << "Enter para continuar...";
-                cin.get();
+                std::cin.ignore();
+                std::cout << "Presione Enter para continuar...";
+                std::cin.get();
                 break;
             case '2':
                 muestraVendedores(listaVendedores, cantVendedores);
-                cin.ignore();
-                cout << "Enter para continuar...";
-                cin.get();
+                std::cin.ignore();
+                std::cout << "Presione Enter para continuar...";
+                std::cin.get();
                 break;
             case '3': 
-                cout << "Teclea el departamento que quieres consultar " << endl;
-                cin >> numDepto;
+                std::cout << "Ingrese el departamento que desea consultar: ";
+                std::cin >> numDepto;
                 vendedoresPorDepartamento(listaVendedores, cantVendedores, listaDeptos, cantDeptos, numDepto);
-                cin.ignore();
-                cout << "\nEnter para continuar...";
-                cin.get();
+                std::cin.ignore();
+                std::cout << "\nPresione Enter para continuar...";
+                std::cin.get();
                 break;
             case '4': 
                 registrarVentas(listaVendedores, cantVendedores);
-                cin.ignore();
-                cout << "Enter para continuar...";
-                cin.get();
+                std::cin.ignore();
+                std::cout << "Presione Enter para continuar...";
+                std::cin.get();
                 break;
             case '5':
-                cout << "Elegiste salir del programa..."<< endl;
+                std::cout << "Has elegido salir del programa." << std::endl;
                 break;
             default:
-                cout << "Opcion invalida"<<endl;
+                std::cout << "Opción inválida." << std::endl;
         }
         system("clear");
     } while (opcion != '5');
